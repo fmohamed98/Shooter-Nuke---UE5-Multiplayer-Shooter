e860849ba474db712778f8c638a111c95bd6ed48
Fayas Mohamed <fayasmkm98@gmail.com>
2026-02-02 16:49:38 +0530

    Adding new Weapon Class & new NukeMap
    Changes :
    Added new map for NukeMap from FPS_LowPolyMap
    Created blueprint for weapon class;
    Changed NukeMap

--- Source/ShooterNuke/Weapon/Weapon.cpp
+++ Source/ShooterNuke/Weapon/Weapon.cpp
@@ -2,13 +2,26 @@
 
 
 #include "Weapon.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 AWeapon::AWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
 
+	m_WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
+	m_WeaponMesh->SetupAttachment(RootComponent);
+
+	m_WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	m_WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	m_WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	m_AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
+	m_AreaSphere->SetupAttachment(RootComponent);
+	m_AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	m_AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +29,11 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority())
+	{
+		m_AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+		m_AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	}
 }
 
 // Called every frame
--- Source/ShooterNuke/Weapon/Weapon.h
+++ Source/ShooterNuke/Weapon/Weapon.h
@@ -6,6 +6,18 @@
 #include "GameFramework/Actor.h"
 #include "Weapon.generated.h"
 
+class USphereComponent;
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	EWS_Initial UMETA(DisplayName = "Inital State"),
+	EWS_Equipped UMETA(DisplayName = "Equipped State"),
+	EWS_Dropped UMETA(DisplayName = "Dropped State"),
+
+	EWS_MAX UMETA(DisplayName = "DefaultMax")
+};
+
 UCLASS()
 class SHOOTERNUKE_API AWeapon : public AActor
 {
@@ -23,4 +35,13 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties", meta = (DisplayName = "Weapon Mesh"))
+	USkeletalMeshComponent* m_WeaponMesh;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties", meta = (DisplayName = "Area Sphere"))
+	USphereComponent* m_AreaSphere;
+
+	UPROPERTY(VisibleAnywhere, meta = (DisplayName = "Weapon State"))
+	EWeaponState m_WeaponState;
 };
